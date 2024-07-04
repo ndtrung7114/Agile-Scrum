accd6152dad501db7ffc000b4a087c15792edafd
ndtrung7114 <trungnd7114@gmail.com>
2024-07-04 13:36:18 +0700

    done day 1 sprint 1

---
+++ CheapDeals.comLTD/login.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace CheapDeals.comLTD
+{
+    partial class login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(83, 272);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(257, 97);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Login ";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(391, -1);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(22, 24);
+            this.label10.TabIndex = 24;
+            this.label10.Text = "X";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 733);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label10;
+    }
+}
---
+++ CheapDeals.comLTD/register.Designer.cs
@@ -0,0 +1,383 @@
+﻿namespace CheapDeals.comLTD
+{
+    partial class register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lb_signup = new System.Windows.Forms.Label();
+            this.lb_signin = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_username = new System.Windows.Forms.TextBox();
+            this.tb_password = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_name = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_email = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb_address = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tb_telephone = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tb_confirm_password = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tb_card_number = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btn_signup = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.cb_showpass = new System.Windows.Forms.CheckBox();
+            this.cb_show_cf_pass = new System.Windows.Forms.CheckBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Silver;
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.cb_show_cf_pass);
+            this.panel1.Controls.Add(this.cb_showpass);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.btn_signup);
+            this.panel1.Controls.Add(this.tb_card_number);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.tb_confirm_password);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.tb_telephone);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.tb_address);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.tb_email);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.tb_name);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.tb_password);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.tb_username);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lb_signin);
+            this.panel1.Controls.Add(this.lb_signup);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(406, 757);
+            this.panel1.TabIndex = 0;
+            // 
+            // lb_signup
+            // 
+            this.lb_signup.AutoSize = true;
+            this.lb_signup.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_signup.ForeColor = System.Drawing.Color.Blue;
+            this.lb_signup.Location = new System.Drawing.Point(233, 133);
+            this.lb_signup.Name = "lb_signup";
+            this.lb_signup.Size = new System.Drawing.Size(64, 21);
+            this.lb_signup.TabIndex = 1;
+            this.lb_signup.Text = "Sign up";
+            // 
+            // lb_signin
+            // 
+            this.lb_signin.AutoSize = true;
+            this.lb_signin.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_signin.Location = new System.Drawing.Point(311, 133);
+            this.lb_signin.Name = "lb_signin";
+            this.lb_signin.Size = new System.Drawing.Size(59, 21);
+            this.lb_signin.TabIndex = 2;
+            this.lb_signin.Text = "Sign in";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(62, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Username:";
+            // 
+            // tb_username
+            // 
+            this.tb_username.Location = new System.Drawing.Point(66, 176);
+            this.tb_username.Multiline = true;
+            this.tb_username.Name = "tb_username";
+            this.tb_username.Size = new System.Drawing.Size(271, 22);
+            this.tb_username.TabIndex = 4;
+            // 
+            // tb_password
+            // 
+            this.tb_password.Location = new System.Drawing.Point(66, 237);
+            this.tb_password.Multiline = true;
+            this.tb_password.Name = "tb_password";
+            this.tb_password.PasswordChar = '*';
+            this.tb_password.Size = new System.Drawing.Size(271, 22);
+            this.tb_password.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Location = new System.Drawing.Point(62, 213);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 21);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password:";
+            // 
+            // tb_name
+            // 
+            this.tb_name.Location = new System.Drawing.Point(66, 394);
+            this.tb_name.Multiline = true;
+            this.tb_name.Name = "tb_name";
+            this.tb_name.Size = new System.Drawing.Size(271, 22);
+            this.tb_name.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(62, 370);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 21);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Name:";
+            // 
+            // tb_email
+            // 
+            this.tb_email.Location = new System.Drawing.Point(66, 455);
+            this.tb_email.Multiline = true;
+            this.tb_email.Name = "tb_email";
+            this.tb_email.Size = new System.Drawing.Size(271, 22);
+            this.tb_email.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.Location = new System.Drawing.Point(62, 431);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 21);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Email:";
+            // 
+            // tb_address
+            // 
+            this.tb_address.Location = new System.Drawing.Point(66, 512);
+            this.tb_address.Multiline = true;
+            this.tb_address.Name = "tb_address";
+            this.tb_address.Size = new System.Drawing.Size(271, 22);
+            this.tb_address.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.Location = new System.Drawing.Point(62, 488);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 21);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Address:";
+            // 
+            // tb_telephone
+            // 
+            this.tb_telephone.Location = new System.Drawing.Point(66, 575);
+            this.tb_telephone.Multiline = true;
+            this.tb_telephone.Name = "tb_telephone";
+            this.tb_telephone.Size = new System.Drawing.Size(271, 22);
+            this.tb_telephone.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.Location = new System.Drawing.Point(62, 551);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(93, 21);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Telephone:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CheapDeals.comLTD.Properties.Resources.obito_80x80;
+            this.pictureBox1.Location = new System.Drawing.Point(159, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(80, 80);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tb_confirm_password
+            // 
+            this.tb_confirm_password.Location = new System.Drawing.Point(66, 317);
+            this.tb_confirm_password.Multiline = true;
+            this.tb_confirm_password.Name = "tb_confirm_password";
+            this.tb_confirm_password.PasswordChar = '*';
+            this.tb_confirm_password.Size = new System.Drawing.Size(271, 22);
+            this.tb_confirm_password.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label7.Location = new System.Drawing.Point(62, 293);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(149, 21);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Confirm Password:";
+            // 
+            // tb_card_number
+            // 
+            this.tb_card_number.Location = new System.Drawing.Point(66, 635);
+            this.tb_card_number.Multiline = true;
+            this.tb_card_number.Name = "tb_card_number";
+            this.tb_card_number.Size = new System.Drawing.Size(271, 22);
+            this.tb_card_number.TabIndex = 18;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label8.Location = new System.Drawing.Point(62, 611);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(113, 21);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Card Number:";
+            // 
+            // btn_signup
+            // 
+            this.btn_signup.BackColor = System.Drawing.Color.Blue;
+            this.btn_signup.FlatAppearance.BorderSize = 0;
+            this.btn_signup.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_signup.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_signup.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btn_signup.Location = new System.Drawing.Point(146, 674);
+            this.btn_signup.Name = "btn_signup";
+            this.btn_signup.Size = new System.Drawing.Size(118, 31);
+            this.btn_signup.TabIndex = 19;
+            this.btn_signup.Text = "Sign Up";
+            this.btn_signup.UseVisualStyleBackColor = false;
+            this.btn_signup.Click += new System.EventHandler(this.btn_signup_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Tahoma", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label9.Location = new System.Drawing.Point(143, 721);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(138, 16);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "I\'m already a member.";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // cb_showpass
+            // 
+            this.cb_showpass.AutoSize = true;
+            this.cb_showpass.Location = new System.Drawing.Point(233, 265);
+            this.cb_showpass.Name = "cb_showpass";
+            this.cb_showpass.Size = new System.Drawing.Size(124, 20);
+            this.cb_showpass.TabIndex = 21;
+            this.cb_showpass.Text = "Show password";
+            this.cb_showpass.UseVisualStyleBackColor = true;
+            this.cb_showpass.CheckedChanged += new System.EventHandler(this.cb_showpass_CheckedChanged);
+            // 
+            // cb_show_cf_pass
+            // 
+            this.cb_show_cf_pass.AutoSize = true;
+            this.cb_show_cf_pass.Location = new System.Drawing.Point(233, 345);
+            this.cb_show_cf_pass.Name = "cb_show_cf_pass";
+            this.cb_show_cf_pass.Size = new System.Drawing.Size(170, 20);
+            this.cb_show_cf_pass.TabIndex = 22;
+            this.cb_show_cf_pass.Text = "Show confirm password";
+            this.cb_show_cf_pass.UseVisualStyleBackColor = true;
+            this.cb_show_cf_pass.CheckedChanged += new System.EventHandler(this.cb_show_cf_pass_CheckedChanged);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(381, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(22, 24);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "X";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // register
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(430, 780);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "register";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox tb_username;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lb_signin;
+        private System.Windows.Forms.Label lb_signup;
+        private System.Windows.Forms.TextBox tb_telephone;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tb_address;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tb_email;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tb_name;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb_password;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_signup;
+        private System.Windows.Forms.TextBox tb_card_number;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox tb_confirm_password;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.CheckBox cb_show_cf_pass;
+        private System.Windows.Forms.CheckBox cb_showpass;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
